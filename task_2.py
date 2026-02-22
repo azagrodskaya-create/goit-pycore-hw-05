7f4336f31f0e1a9889300031c5cf799c0ff53454
Anna Zagrodska <azagrodskaya@gmail.com>
2026-02-22 12:13:45 +0200

    task_2 upd

--- task_2.py
+++ task_2.py
@@ -3,14 +3,14 @@ from typing import Callable, Generator
 
 
 def generator_numbers(text: str) -> Generator[float, None, None]:
-    # аналізує текст та знаходить всі дійсні числа, що відокремлені пробілами.
+    # Аналізує текст та знаходить всі дійсні числа, що відокремлені пробілами.
     pattern = r'\b\d+\.\d+\b'
-    for match in re.finditer(pattern, text): # знаходить всі збіги в тексті за допомогою регулярного виразу
-        yield float(match.group()) # перетворює знайдені рядки на числа з плаваючою комою та повертає їх по одному
+    for match in re.finditer(pattern, text): # Знаходить всі збіги в тексті за допомогою регулярного виразу
+        yield float(match.group()) # Перетворює знайдені рядки на числа з плаваючою комою та повертає їх по одному
 
 
 def sum_profit(text: str, func: Callable[[str], Generator[float, None, None]]) -> float:
-    # підсумовує всі знайдені числа від генератора
+    # Підсумовує всі знайдені числа від генератора
     total_sum = 0.0
     # Ітеруємося по генератору, який повертає func(text)
     for number in func(text):
